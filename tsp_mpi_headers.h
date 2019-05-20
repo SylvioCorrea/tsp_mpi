d84bb003ec9c0848a08878c64a7f09c4b33f8e1f
SylvioCorrea <31700840+SylvioCorrea@users.noreply.github.com>
2019-05-20 16:22:32 -0300

    Update tsp_mpi_headers.h

--- tsp_mpi_headers.h
+++ tsp_mpi_headers.h
@@ -4,8 +4,8 @@
 #include <string.h>
 #include <math.h>
 #include <float.h>
-#include <omp.h>
 #include <time.h>
+#include "mpi.h"
 
 //Defines the number of cities to be used in the problem
 #define N_OF_CS 14
@@ -47,7 +47,8 @@ double calc_length(int path[], double distance_m[N_OF_CS][N_OF_CS]);
 void print_distance_m(double *distance_m, int n);
 void print_distance_m2(double distance_m[N_OF_CS][N_OF_CS]);
 
-void tsp(double distance_m[N_OF_CS][N_OF_CS], int start);
+void master_routine(Message *message_ptr, int available[],
+                    int best_path[], int path_size, int *burst);
 void tsp_aux(int path[], int path_size, int available[],
              double distance_m[N_OF_CS-1][N_OF_CS],
              int best_path[], double *best_length);
