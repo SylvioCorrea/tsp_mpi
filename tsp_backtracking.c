acb91282f8e22a889c47e3a1f7f969c57115d1ce
Sylvio Correa <sylvio.acad@acad.pucrs.br>
2019-05-21 16:28:16 -0300

    another push

--- tsp_backtracking.c
+++ tsp_backtracking.c
@@ -45,7 +45,7 @@ void master_routine(Message *message_ptr, int available[], int best_path[],
         if((*burst) < proc_n) {
             //First burst of jobs is sent with no need for slave request.
             MPI_Send(message_ptr, sizeof(Message), MPI_BYTE, (*burst), WORK, MPI_COMM_WORLD);
-            printf("job %d sent\n", *burst);
+            //printf("job %d sent\n", *burst);
             (*burst)++;
             jobs++;
         } else {
@@ -60,7 +60,7 @@ void master_routine(Message *message_ptr, int available[], int best_path[],
                 //A better path has been found.
                 //Save it's length.
                 message_ptr->best_length = results.best_length;
-                printf("*[M]: %.2f\n", message_ptr->best_length);
+                //printf("*[M]: %.2f\n", message_ptr->best_length);
                 //Copy path.
                 for(i=0; i<N_OF_CS; i++) {
                     best_path[i] = results.path[i];
@@ -92,7 +92,7 @@ void tsp_aux(int path[], int path_size, int available[],
         double path_length = calc_length(path, distance_m);
         if(path_length < *best_length) {
             *best_length = path_length;
-            printf("[s]: %.2f\n", *best_length);
+            //printf("[s]: %.2f\n", *best_length);
             copy_path(path, best_path);
         }
     } else {
@@ -201,7 +201,7 @@ int main(int argc, char **argv) {
                 //A better path has been found.
                 //Save it's length.
                 message.best_length = results.best_length;
-                printf("final check: %.2f\n", message.best_length);
+                //printf("final check: %.2f\n", message.best_length);
                 //Copy path.
                 for(i=0; i<N_OF_CS; i++) {
                     best_path[i] = results.path[i];
